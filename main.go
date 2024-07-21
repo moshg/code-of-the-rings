b4223512dcb85dac916cf84538a25ace1c9c99e8
mosh <mosh.ohayo@gmail.com>
2024-07-21 17:03:28 +0900

    Add trivial implementation

--- main.go
+++ main.go
@@ -6,19 +6,55 @@ import (
 	"os"
 )
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
-
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Buffer(make([]byte, 1000000), 1000000)
-
+	// magicPhrase contains between 1 and 500 characters.
+	scanner.Buffer(make([]byte, 500), 500)
 	scanner.Scan()
 	magicPhrase := scanner.Text()
-	_ = magicPhrase // to avoid unused error
 
-	// fmt.Fprintln(os.Stderr, "Debug messages...")
-	fmt.Println("+.>-.") // Write action to stdout
+	magicPhraseAsIntegers := magicPhraseToIntegers(magicPhrase)
+	instructions := solution(magicPhraseAsIntegers)
+
+	fmt.Println(instructions)
+}
+
+func magicPhraseToIntegers(magicPhrase string) []int {
+	var magicPhraseAsIntegers []int
+	for _, char := range magicPhrase {
+		if char == ' ' {
+			magicPhraseAsIntegers = append(magicPhraseAsIntegers, 0)
+		} else {
+			magicPhraseAsIntegers = append(magicPhraseAsIntegers, int(char)-int('A')+1)
+		}
+	}
+	return magicPhraseAsIntegers
+}
+
+func solution(magicPhrase []int) string {
+	var instructions string
+
+	for _, letter := range magicPhrase {
+		if letter == 0 {
+			instructions += "."
+		} else if letter <= 13 {
+			for i := 0; i < letter; i++ {
+				instructions += "+"
+			}
+			instructions += "."
+			for i := 0; i < letter; i++ {
+				instructions += "-"
+			}
+		} else {
+			for i := 0; i < 26-letter+1; i++ {
+				instructions += "-"
+			}
+			instructions += "."
+			for i := 0; i < 26-letter+1; i++ {
+				instructions += "+"
+			}
+		}
+	}
+
+	return instructions
 }
